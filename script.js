670a9000a25fbcaef4bde9dfad1ff98c51b0f77b
Anubhav Ashish <anubhav.ashish9@gmail.com>
2017-09-24 23:09:00 +1000

    24Sep17

---
+++ script.js
@@ -0,0 +1,38 @@
+/* 
+    Lecture: Variables
+    
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+var age = 26;
+
+console.log(age);
+
+var fullAge = false;
+console.log(fullAge);
+
+*/
+
+// Lecture: Variables 2
+
+var name = 'John';
+var age = 26;
+
+console.log(name+age);
+var job, isMarried; // These variables are currently undefined
+
+console.log(job);
+job = 'Engineer';
+isMarried = false;
+
+console.log(job+isMarried);
+
+age = 'Thirty Six';
+job = 'Shopper';
+
+console.log(job+isMarried);
+var lastName = prompt("What is the last name?");
+console.log(lastName);
+alert('Your name is: '+lastName);
