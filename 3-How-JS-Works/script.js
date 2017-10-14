7309ff276e20609f7d2e1258cd5b45c5da3a3c48
Anubhav Ashish <anubhav.ashish9@gmail.com>
2017-10-14 23:28:10 +1100

    The this keyword!

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -67,11 +67,32 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//console.log(this);
 
+var year = 1994;
+calculateAge(year);
+function calculateAge(year){
+    console.log(2016-year);
+    console.log(this);
+}
 
+var john = {
+    name: "John",
+    yearOfBirth: 1990,
+    calculateAge : function(){
+        console.log(2016-this.yearOfBirth);
+    }
+}
+john.calculateAge();
 
+var mike = {
+    name: "Mike",
+    yearOfBirth: 1984    
+};
 
+mike.calculateAge = john.calculateAge;
 
+mike.calculateAge();
 
 
 
